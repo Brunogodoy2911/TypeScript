b350d6041dd7107a081d544b9d293fed767ce287
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-01-14 02:16:34 -0300

    Inferencia de Tipos e Tipagme Explicita

---
+++ Tipos primitivos no Typescript/InferenciaDeTiposETipagemExplicita.ts
@@ -0,0 +1,13 @@
+// Tipagem explicita
+
+let myName: string;
+
+myName = "Bruno H.";
+
+// Inferência de Tipos
+
+let message = "Oi, tudo bem?";
+
+// message = 74 => O tipo 'number' não pode ser atribuído ao tipo 'string'.
+
+message = "Sim, tudo ótimo!";
