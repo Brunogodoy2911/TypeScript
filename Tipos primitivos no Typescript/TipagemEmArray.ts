522899c9ca5f70fda882da5cfa229eacd2218917
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-01-14 02:28:05 -0300

    Tipagem em Arrays

---
+++ Tipos primitivos no Typescript/TipagemEmArray.ts
@@ -0,0 +1,8 @@
+// Tipagem explicita ou Anotação de tipo (Type Annotation)
+
+let names: string[] = ["Bruno", "Rodrigo"];
+let numbers: number[] = [1, 2, 3, 4, 5];
+
+// Inferência de tipos.
+
+let products = ["Produto x", "Produto y", "Produto z"]; // let products: string[]
