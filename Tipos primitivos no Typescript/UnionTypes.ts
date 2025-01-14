da51b25620c574352111c6252b4caef6578a1ba9
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-01-14 02:58:17 -0300

    Union/Types

---
+++ Tipos primitivos no Typescript/UnionTypes.ts
@@ -0,0 +1,7 @@
+let response: string | null | number;
+
+response = "Teste";
+response = null;
+response = 98;
+
+// response = true => O tipo 'true' não pode ser atribuído ao tipo 'string | number | null';
