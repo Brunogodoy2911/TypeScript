bb68955b836d97f9a14a9a914a22714856ed7ae3
Bruno Godoy <105827102+Brunogodoy2911@users.noreply.github.com>
2025-01-14 02:05:36 -0300

    Tipos Primitivos em TS

---
+++ Tipos primitivos no Typescript/TiposPrimitivos.ts
@@ -0,0 +1,19 @@
+// String para TEXTO!
+
+let username: string;
+
+username = "Bruno";
+
+//Number para NÚMERO!
+
+let total: number;
+
+total = 3;
+total = 7.5;
+
+//Boolean para VERDADEIRO OU FALSO!
+
+let isLoading: boolean;
+
+isLoading = true;
+isLoading = false;
