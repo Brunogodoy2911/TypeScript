ca0d02a19eca1b36b276be387a59e32f3297a594
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-01-14 02:55:23 -0300

    Null/Undefined

---
+++ Tipos primitivos no Typescript/NullUndefined.ts
@@ -0,0 +1,21 @@
+// undefined
+
+let value: number;
+
+// console.log(value) => undefined;
+
+let user = {
+  name: "Bruno",
+};
+
+// console.log(user.email) => undefined
+
+// null
+
+let email = null;
+
+console.log(email); // null
+
+if (!email) {
+  console.log("Informe o e-mail!");
+}
